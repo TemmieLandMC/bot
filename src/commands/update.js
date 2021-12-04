22e10aff3b00d10aa24692f91d02b6569ea1f022
Street02krutoy <masaanekdot@gmail.com>
2021-12-04 13:39:00 +0300

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -11,11 +11,32 @@ module.exports.run = async (interaction = new CommandInteraction) => {
     exec("git stash push --include-untracked");
     exec("git pull", (error, stdout) => {
         exec("git stash drop");
-        if (error) return interaction.reply(`\`\`\`fix\n${error}\n\`\`\``);
+        if (error) return interaction.reply({
+            content: `\`\`\`fix\n${error}\n\`\`\``, "components": [
+                {
+                    type: 2,
+                    emoji: {
+                        name: "🗑"
+                    },
+                    style: 4,
+                    custom_id: "reply:delete"
+                }
+            ]
+        });
 
         if (stdout.includes("Already up to date.")) {
             interaction.reply({
-                content: "Bot already up to date. No changes since last pull."
+                content: "Bot already up to date. No changes since last pull.",
+                "components": [
+                    {
+                        type: 2,
+                        emoji: {
+                            name: "🗑"
+                        },
+                        style: 4,
+                        custom_id: "reply:delete"
+                    }
+                ]
             });
         } else {
             interaction.reply({
