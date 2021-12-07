da949b0f649ecd448bc54d488bc2477c5c954472
itzJOHv <kaktusgame806@gmail.com>
2021-12-07 18:43:59 +0200

    Update search.js

--- src/commands/search.js
+++ src/commands/search.js
@@ -2,45 +2,40 @@ module.exports = {
     name: "search",
     permissionRequired: 0,
     slash: true,
-    opts: [
-        {
-            name: "query",
-            description: "Что вы хотите найти",
-            type: 3,
-            required: true,
-        },
-    ]
+    opts: [{
+        name: "query",
+        description: "Что вы хотите найти",
+        type: 3,
+        required: true,
+    }]
 };
 
 const { CommandInteraction } = require("discord.js");
 const axios = require("axios");
 
 module.exports.run = async (interaction = new CommandInteraction) => {
-    const query = interaction.options.getString("query");
-    axios.get("https://google.com/search", { params: { "q": query } }).then(r => {
+    const q = interaction.options.getString("query");
+
+    axios.get("https://google.com/search", { params: { q } }).then((r) => {
         interaction.reply({
             content: "✅ Ваш запрос: ",
-            "components": [
+            components: [{
+                type: 1,
+                components: [{
+                    type: 2,
+                    label: "Ссылка",
+                    style: 5,
+                    url: "https://google.com" + r.request.path
+                },
                 {
-                    "type": 1,
-                    "components": [
-                        {
-                            type: 2,
-                            "label": "Ссылка",
-                            "style": 5,
-                            "url": "https://google.com" + r.request.path
-                        },
-                        {
-                            type: 2,
-                            emoji: {
-                                name: "🗑"
-                            },
-                            style: 4,
-                            custom_id: "reply:delete"
-                        }
-                    ]
-                }
-            ]
-        })
-    })
+                    type: 2,
+                    emoji: {
+                        name: "🗑"
+                    },
+                    style: 4,
+                    custom_id: "reply:delete"
+                }]
+            }]
+        });
+    });
 };
